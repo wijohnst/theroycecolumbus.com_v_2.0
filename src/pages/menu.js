cbb4b38cbef22b8fe1fbf414290f52fd8767496e
Will Johnston <wijohnst@gmail.com>
2020-06-16 10:40:52 -0400

    connected json data to menu page

---
+++ src/pages/menu.js
@@ -0,0 +1,20 @@
+import React from "react"
+import JSONData from "../../src/content/menu.json"
+
+const menu = () => (
+  <div style={{ maxWidth: `960px`, margin: `1.45rem` }}>
+    <h1>{JSONData.title}</h1>
+    <h2>Appetizers</h2>
+    <ul>
+      {JSONData.appetizers_list.map((data, index) => {
+        return <li key={`appetizer_${index}`}>{data.app_name}
+          <ul>
+            <li>{data.app_description}</li>
+            <li>{data.app_price}</li>
+          </ul>
+        </li>
+      })}
+    </ul>
+  </div>
+)
+export default menu
