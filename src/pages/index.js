31d1c21f51f5f237b7f9f6577d290c1762adf4ae
Will Johnston <wijohnst@gmail.com>
2020-06-21 10:13:35 -0400

    added hero image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,39 @@
 import React from "react"
-// import styled from 'styled-components'
+import { useStaticQuery, graphql } from 'gatsby'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Img from "gatsby-image"
 
 export default function IndexPage(){
 
+  const data = useStaticQuery(graphql`
+  query MyQuery {
+    file(relativePath: {eq: "cocktail.jpg"}) {
+      childImageSharp {
+        fluid {
+          aspectRatio
+          base64
+          sizes
+          src
+          srcSet
+        }
+      }
+    }
+  }
+  
+  `)
   return(
     <Layout>
     <SEO title="Home" />
-    
+    <Img 
+      fluid={data.file.childImageSharp.fluid}
+      alt="A cocktail."
+    />
   </Layout>
   )
 }
+
 
 
 
